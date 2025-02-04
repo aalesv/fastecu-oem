bbb0ebb2d430c3e9a76f2c02fa051a002709dd76
aalesv <aalesv70@hotmail.com>
2025-02-04 14:08:44 +0300

    Added sequence number to network packets

--- serial_port/websocketiodevice.cpp
+++ serial_port/websocketiodevice.cpp
@@ -7,16 +7,25 @@
 
 #include "websocketiodevice.h"
 
+template <typename NUMBER>
+QByteArray toQByteArray(NUMBER number)
+{
+    QBuffer buf;
+    buf.open(QBuffer::ReadWrite);
+    QDataStream stream(&buf);
+
+    stream<<number;
+    QByteArray arr=buf.buffer();
+    return arr;
+}
+
 WebSocketIoDevice::WebSocketIoDevice(QWebSocket *webSocket, QObject *parent)
     : QIODevice(parent)
     , m_socket(webSocket)
 {
     open(QIODevice::ReadWrite);
     connect(webSocket, &QWebSocket::disconnected, this, &WebSocketIoDevice::disconnected);
-    connect(webSocket, &QWebSocket::binaryMessageReceived, this, [this](const QByteArray &message){
-        m_buffer.append(message);
-        emit readyRead();
-    });
+    connect(webSocket, &QWebSocket::binaryMessageReceived, this, &WebSocketIoDevice::binaryMessageReceived);
     connect(webSocket, &QWebSocket::bytesWritten, this, &WebSocketIoDevice::bytesWritten);
 }
 
@@ -49,6 +58,113 @@ qint64 WebSocketIoDevice::readData(char *data, qint64 maxlen)
 qint64 WebSocketIoDevice::writeData(const char *data, qint64 len)
 {
     if (m_socket)
-        return m_socket->sendBinaryMessage(QByteArray{data, int(len)});
+    {
+        QByteArray message = QByteArray{data, int(len)};
+        SEQ_TX++;
+        prependHeader(message);
+        return m_socket->sendBinaryMessage(message);
+    }
     return -1;
+}
+
+void WebSocketIoDevice::binaryMessageReceived(const QByteArray &message)
+{
+    bool pass_message = true;
+    QByteArray m = message;
+    sequence_t seqRx = getSequenceNumber(message);
+    //qDebug() << "SEQ_RX" << SEQ_RX << "seqRx" << seqRx;
+    if (isHeaderValid(m))
+    {
+        if (!isSequenceValid(message))
+        {
+            qDebug() << "Last RX SEQ was" << SEQ_RX
+                     << "but received" << seqRx;
+            pass_message = false;
+        }
+        else
+        {
+            SEQ_RX = seqRx;
+        }
+        removeHeader(m);
+    }
+    else
+        qDebug() << "WebSocketIoDevice: Invalid header";
+    m_buffer.append(m);
+    emit readyRead();
+}
+
+WebSocketIoDevice::checksum_t WebSocketIoDevice::calculateChecksum(const QByteArray &message)
+{
+    checksum_t checksum = 0;
+    for(int i=0; i<message.size(); i++)
+        checksum += (checksum_t)message[i];
+    return ~checksum;
+}
+
+bool WebSocketIoDevice::isHeaderValid(const QByteArray &message)
+{
+    int len = message.size();
+    if (len < headerLength)
+        return false;
+    QByteArray header = message.chopped(len-headerLength+1);
+    checksum_t checksum_c = calculateChecksum(header);
+    checksum_t checksum_p = (checksum_t)message[headerLength-1];
+    if (checksum_c != checksum_p)
+        return false;
+    else
+        return true;
+}
+
+bool WebSocketIoDevice::isSequenceValid(const QByteArray &message)
+{
+    bool r = true;
+    sequence_t seqRx = getSequenceNumber(message);
+    if (SEQ_RX == 0 || seqRx == 1)
+    {
+        //If we just started or remote side just started,
+        //set sequnce number in any case
+        r = true;
+    }
+    else
+    {
+        if (SEQ_RX >= seqRx)
+        {
+            r = false;
+        }
+    }
+    return r;
+}
+
+bool WebSocketIoDevice::prependHeader(QByteArray &message)
+{
+    QByteArray header;
+    header.append(toQByteArray(headerLength));
+    header.append(toQByteArray(protocolVersion));
+    header.append(toQByteArray(SEQ_TX));
+    header.append(calculateChecksum(header));
+    message.prepend(header);
+    return true;
+}
+
+bool WebSocketIoDevice::removeHeader(QByteArray &message)
+{
+    int len = message.size();
+    if (len < headerLength)
+        return false;
+    message = message.remove(0, headerLength);
+    return true;
+}
+
+WebSocketIoDevice::sequence_t WebSocketIoDevice::getSequenceNumber(const QByteArray &message)
+{
+    if (message.size()<headerLength)
+        return 0;
+    //Remove length and version fields
+    QByteArray b = message;
+    b.remove(0, sizeof(int_t) + sizeof(int_t));
+    b.remove(sizeof(sequence_t), b.size()-sizeof(sequence_t));
+    QDataStream ds(b);
+    sequence_t seq;
+    ds >> seq;
+    return seq;
 }
--- serial_port/websocketiodevice.h
+++ serial_port/websocketiodevice.h
@@ -11,6 +11,9 @@
 
 class WebSocketIoDevice : public QIODevice
 {
+    using sequence_t = quint64;
+    using checksum_t = uint8_t;
+    using int_t = uint8_t;
     Q_OBJECT
 public:
     WebSocketIoDevice(QWebSocket *webSocket, QObject *parent = nullptr);
@@ -27,8 +30,23 @@ public:
 protected:
     qint64 readData(char *data, qint64 maxlen) override;
     qint64 writeData(const char *data, qint64 len) override;
+    void binaryMessageReceived(const QByteArray &message);
 
 private:
+    //Protocol structure
+    //[Length with checksum][Version][Header][Checksum]
+    //For version 1 header is [sequence number]
+    const int_t protocolVersion = 1;
+    const int_t headerLength =
+        sizeof(int_t) + sizeof(int_t) + sizeof(sequence_t) + sizeof(checksum_t);
+    sequence_t SEQ_TX = 0;
+    sequence_t SEQ_RX = 0;
+    bool prependHeader(QByteArray &message);
+    bool removeHeader(QByteArray &message);
+    checksum_t calculateChecksum(const QByteArray &message);
+    bool isHeaderValid(const QByteArray &message);
+    bool isSequenceValid(const QByteArray &message);
+    sequence_t getSequenceNumber(const QByteArray &message);
     QPointer<QWebSocket> m_socket;
     QByteArray m_buffer;
 };
